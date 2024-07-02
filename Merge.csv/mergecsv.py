98eb4bc8261bf7803e79b08233ebc94a7363963b
Beteab <beteabtefera@yahoo.com>
2024-07-01 19:31:45 -0500

    mergeCSV Files

--- Merge.csv/mergecsv.py
+++ Merge.csv/mergecsv.py
@@ -6,8 +6,37 @@ challenge:
         - list of input files(list), path outputfile
         - function should be robust enough to merge even though headers don't match
 '''
+import pandas as pd
 import csv
-filePath = 'C:/Users/bebid/Desktop/Projects/Projects/Merge/sample1.csv'
-with open(filePath,'r') as file:
-    file = csv(file)
-    file.reader()
+filePath = 'C:/Users/bebid/Desktop/Projects/Projects/Merge.csv/sample1.csv'
+filePath2 = 'C:/Users/bebid/Desktop/Projects/Projects/Merge.csv/sample2.csv'
+newfilePath = 'C:/Users/bebid/Desktop/Projects/Projects/Merge.csv/output.csv'
+# df = pd.read_csv(filePath)
+
+# print(df['Name'][0])
+
+def merege_csv(list_file:list,newfilePath='output.csv'):
+    headers_set = set()
+
+    #get headers for each file
+    for file in list_file:
+        with open(file,'r') as file:
+            csv_reader =  csv.reader(file)
+            headers = next(csv_reader)
+            headers_set.update(headers)
+
+    headers_list = sorted(headers_set)
+    #open the new file and insert new records
+    with open(newfilePath,'w') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(headers_list)
+    
+        for file in list_file:
+            with open(file, 'r') as f:
+                csv_reader = csv.DictReader(f)
+                for row in csv_reader:
+                    merge_row = {header: row.get(header,'') for header in headers_list}
+                    csv_writer.writerow(merge_row.values())
+    print(f'Merged CSV saved to {newfilePath}')
+
+print(merege_csv([filePath,filePath2],newfilePath))
